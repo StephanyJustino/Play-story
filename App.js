2684baf16ef91037d8517d4ea732bdb4aa4c9b8f
Stephany Justino <juliajustinoetec@gmail.com>
2022-03-29 10:52:11 -0300

    parte 2 do projeto

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import Cabecalho from "./src/components/cabecalho";
 import Botao from "./src/components/botao";
-
+import Dados from "./dados/componentes/componente/dadosJogos";
+import Jogos from "./dados/Jogos";
 
 export default function App() {
   return (
@@ -16,6 +17,20 @@ export default function App() {
       texto2="Favoritos"
       cor2="black"
       />
+      <View>
+        <FlatList
+        horizontal={true}
+        data = {Dados}
+        keyExtractor = {(item) => item.id}
+        renderItem = { ({ item }) => (
+          <Jogos
+          titulo= {item.nome}
+          imagem= {item.imagem}
+          valor= {item.valor}
+          />
+        )}
+        />
+      </View>
      </View>
   );
 }
